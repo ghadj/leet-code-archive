1ca4425d5a85e518cd3a0d7ef099e392ba66deb0
George Hadjiantonis <georgehadjiantonis@hotmail.com>
2025-06-10 01:29:05 +0300

    Reorganize readme per sub-directory

--- add-two-numbers/Solution.java
+++ add-two-numbers/Solution.java
@@ -1,22 +1,4 @@
-/**
- * ---
- * [https://leetcode.com/problems/add-two-numbers/]
- * 2. Add Two Numbers
- * You are given two non-empty linked lists representing two non-negative
- * integers. The digits are stored in reverse order, and each of their nodes
- * contains a single digit. Add the two numbers and return the sum as a linked
- * list.
- * ---
- *
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+
 class Solution {
 
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
